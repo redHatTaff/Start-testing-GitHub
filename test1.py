16d11468a7fa865c30e7ce7148c030dd94da75ec
root <taffmpaz@gmail.com>
2018-10-11 11:04:48 +0200

    Changed test1.py

---
+++ test1.py
@@ -0,0 +1,14 @@
+print('Hello world!')
+firstName= input('What\'s yall name?')
+print('Your first name is', firstName)
+
+def function1():
+    language = input('What\'s your language?')
+    if language == 'Eng':
+        print('What is up, Dwag?')
+
+    elif language == 'Span':
+        print('Bounjour')
+
+    else:
+        print('hey man')
